b5a63e6e540481cc0b28bbbf79d4a7c8a5ca1732
saswat <nandasaswat1999@gmail.com>
2019-09-19 22:48:15 +0530

    Update Name_Scraper.py

--- Name_Scraper.py
+++ Name_Scraper.py
@@ -2,19 +2,23 @@
 # coding: utf-8
 
 # In[31]:
-
+#This code lists out the GPU names from newegg website
 
 import requests
 from bs4 import BeautifulSoup 
 
 url = "https://www.newegg.com/global/in-en/p/pl?d=graphics+card"
 
+#grabs the webpage
 page = requests.get(url, timeout = 5)
 
+#parsing html
 soup = BeautifulSoup(page.content, "html.parser")
 
+#finds all of the relative tags and sets ground for looping
 container = soup.findAll("div",{"class":"item-info"})
 
+#loop to grab names
 for contains in container:
     x = contains.findAll("a", {"class":"item-title"})
     product_name = x[0].text
